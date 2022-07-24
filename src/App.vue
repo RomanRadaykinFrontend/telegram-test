039104342c40eb86d5739459da52adfc6692b46c
Роман Радайкин <romanradajkin@MacBook-Pro-Roman.local>
2022-07-24 18:20:52 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,27 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-telegram-login
+        mode="callback"
+        telegram-login="tresGoldTestBot"
+        @callback="yourCallbackFunction"
+    />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { vueTelegramLogin } from 'vue-telegram-login'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, vueTelegramLogin
+  },
+  methods:{
+    yourCallbackFunction (e) {
+      console.log(e)
+    },
   }
 }
 </script>
